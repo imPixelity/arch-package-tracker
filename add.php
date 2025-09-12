d3e0b76fd685508e9f4497be572e2b9107162893
Dzikry <dzikrynp14@gmail.com>
2025-09-12 22:41:39 +0700

    feat: style add.php

--- add.php
+++ add.php
@@ -1,19 +1,65 @@
-<form action="add-process.php" method="post">
-    <label>Package Name:
-        <input type="text" name="package-name">
-    </label>
-    <br>
-    <label>Category:
-        <input type="text" name="category">
-    </label>
-    <br>
-    <label>Source:
-        <input type="text" name="source">
-    </label>
-    <br>
-    <label>Notes:
-        <input type="text" name="notes">
-    </label>
-    <br>
-    <input type="submit" value="Submit">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets/img/favicon.png">
+    <link rel="stylesheet" href="assets/css/add.css">
+    <title>Arch Package Tracker</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="wrapper">
+            <form action="add-process.php" method="post">
+                <table>
+                    <tr>
+                        <td>
+                            <label for="package-name" class="required">Package Name</label>
+                        </td>
+                        <td>
+                            <input type="text" name="package-name" id="package-name" required>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="category">Category</label>
+                        </td>
+                        <td>
+                            <input type="text" name="category" id="category">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="source" class="required">Source</label>
+                        </td>
+                        <td>
+                            <input type="text" name="source" id="source" required>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="notes">Notes</label>
+                        </td>
+                        <td>
+                            <input type="text" name="notes" id="notes">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><a href="index.php" class="back-button">&larr; Back</a></td>
+                        <td>
+                            <input type="submit" value="Submit" class="submit-button">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
